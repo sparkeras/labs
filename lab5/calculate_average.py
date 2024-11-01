671d7e6b71c89b0961125b039ed9044bbc0e701b
sparkeras <asparkerzerofive@gmail.com>
2024-11-01 00:19:01 +0000

    lab tasks complete

--- lab5/calculate_average.py
+++ lab5/calculate_average.py
@@ -18,10 +18,16 @@ def calculate_average(numbers):
     """
     
     # Function implementation here ...
+    
+    count = 0
+    numsum = 0
+    for x in numbers:
+        numsum = numsum + x 
+        count = count + 1
 
-
+    average = numsum/count
     return average
 
 # # Example usage
-# numbers = [10, 20, 30, 40, 50]
-# print("The average is:", calculate_average(numbers))  # Expected output: The average is: 30.0
+numbers = [20, 30, 40, 50, 60, 70, 80]
+print("The average is:", calculate_average(numbers))  # Expected output: The average is: 50.0
